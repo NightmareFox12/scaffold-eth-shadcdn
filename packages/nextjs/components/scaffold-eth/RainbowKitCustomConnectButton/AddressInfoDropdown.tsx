7ee1d4f6b17341b7ff52bff8b3610c4413107239
NightmareFox <carloshenriquez392004@gmail.com>
2025-04-27 10:40:24 -0400

    Refactor AddressInfoDropdown to simplify component structure, enhance button styling, and improve user interaction for address copying, QR code viewing, and network switching. Remove unused hooks and streamline state management.

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/AddressInfoDropdown.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/AddressInfoDropdown.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { AddressQRCodeModal } from "./AddressQRCodeModal";
 import { NetworkOptions } from "./NetworkOptions";
 import { LogOut } from "lucide-react";
@@ -6,10 +6,9 @@ import CopyToClipboard from "react-copy-to-clipboard";
 import { getAddress } from "viem";
 import { Address } from "viem";
 import { useDisconnect } from "wagmi";
+import { ArrowsRightLeftIcon } from "@heroicons/react/20/solid";
 import {
-  ArrowLeftOnRectangleIcon,
   ArrowTopRightOnSquareIcon,
-  ArrowsRightLeftIcon,
   CheckCircleIcon,
   ChevronDownIcon,
   DocumentDuplicateIcon,
@@ -18,7 +17,6 @@ import {
 import { BlockieAvatar, isENS } from "~~/components/scaffold-eth";
 import { Button } from "~~/components/shad/button";
 import { Popover, PopoverContent, PopoverTrigger } from "~~/components/shad/popover";
-import { useOutsideClick } from "~~/hooks/scaffold-eth";
 import { getTargetNetworks } from "~~/utils/scaffold-eth";
 
 const allowedNetworks = getTargetNetworks();
@@ -44,102 +42,26 @@ export const AddressInfoDropdown = ({
   const [selectingNetwork, setSelectingNetwork] = useState<boolean>(false);
   const [qrCodeModalOpen, setQrCodeModalOpen] = useState<boolean>(false);
 
-  const dropdownRef = useRef<HTMLDetailsElement>(null);
-  const closeDropdown = () => {
-    setSelectingNetwork(false);
-    dropdownRef.current?.removeAttribute("open");
-  };
-  useOutsideClick(dropdownRef, closeDropdown);
-
   return (
     <>
       <AddressQRCodeModal address={address} qrCodeModalOpen={qrCodeModalOpen} setQrCodeModalOpen={setQrCodeModalOpen} />
 
       <Popover>
         <PopoverTrigger asChild>
-          <Button className="">
+          <Button>
             <BlockieAvatar address={checkSumAddress} size={30} ensImage={ensAvatar} />
             <span className="ml-2 mr-1">
               {isENS(displayName) ? displayName : checkSumAddress?.slice(0, 6) + "..." + checkSumAddress?.slice(-4)}
             </span>
-            <ChevronDownIcon className="h-6 w-4 ml-2 sm:ml-0" />
+            <ChevronDownIcon className="h-4 w-4" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent side="bottom" className="p-2  me-4">
+
+        <PopoverContent side="bottom" className="p-2 me-4 max-w-56 flex flex-col justify-start items-start">
           <NetworkOptions hidden={!selectingNetwork} />
 
-          <Button variant="ghost" className="w-full">
-            <CopyToClipboard
-              text={checkSumAddress}
-              onCopy={() => {
-                setAddressCopied(true);
-                setTimeout(() => {
-                  setAddressCopied(false);
-                }, 800);
-              }}
-            >
-              <div className="flex flex-1 items-center gap-2">
-                {!addressCopied ? (
-                  <>
-                    <DocumentDuplicateIcon className=" h-4 w-4 cursor-pointer ml-2 sm:ml-0" aria-hidden="true" />
-                    <span className=" whitespace-nowrap">Copy address</span>
-                  </>
-                ) : (
-                  <>
-                    <CheckCircleIcon className=" h-4 w-4 cursor-pointer ml-2 sm:ml-0" aria-hidden="true" />
-                    <span className=" whitespace-nowrap">Copied</span>
-                  </>
-                )}
-              </div>
-            </CopyToClipboard>
-          </Button>
-
-          <Button variant="ghost" className="w-full" onClick={() => setQrCodeModalOpen(true)}>
-            <div className="flex flex-1 items-center gap-2">
-              <QrCodeIcon className="h-6 w-4 ml-2 sm:ml-0" />
-              <span className="whitespace-nowrap">View QR Code</span>
-            </div>
-          </Button>
-
-          <Button variant="ghost" className="w-full" onClick={() => disconnect()}>
-            <div className="flex flex-1 items-center gap-2">
-              <LogOut className="h-4 w-4 stroke-error" />
-              <span className="text-error">Disconnect</span>
-            </div>
-          </Button>
-
-          <Button variant="ghost" className="w-full" onClick={() => disconnect()}>
-            <div className="flex flex-1 items-center gap-2">
-              <LogOut className="h-4 w-4 stroke-error" />
-              <span className="text-error">Disconnect</span>
-            </div>
-          </Button>
-        </PopoverContent>
-      </Popover>
-
-      {/* <details ref={dropdownRef} className="dropdown dropdown-end leading-3">
-        <summary tabIndex={0} className="btn btn-secondary btn-sm pl-0 pr-2 shadow-md dropdown-toggle gap-0 !h-auto">
-          <BlockieAvatar address={checkSumAddress} size={30} ensImage={ensAvatar} />
-          <span className="ml-2 mr-1">
-            {isENS(displayName) ? displayName : checkSumAddress?.slice(0, 6) + "..." + checkSumAddress?.slice(-4)}
-          </span>
-          <ChevronDownIcon className="h-6 w-4 ml-2 sm:ml-0" />
-        </summary>
-        <ul
-          tabIndex={0}
-          className="dropdown-content menu z-[2] p-2 mt-2 shadow-center shadow-accent bg-base-200 rounded-box gap-1"
-        >
-          <NetworkOptions hidden={!selectingNetwork} />
-          <li className={selectingNetwork ? "hidden" : ""}>
-            {addressCopied ? (
-              <div className="btn-sm !rounded-xl flex gap-3 py-3">
-                <CheckCircleIcon
-                  className="text-xl font-normal h-6 w-4 cursor-pointer ml-2 sm:ml-0"
-                  aria-hidden="true"
-                />
-                <span className=" whitespace-nowrap">Copy address</span>
-              </div>
-            ) : (
+          {!selectingNetwork && (
+            <Button variant="ghost" className="w-full">
               <CopyToClipboard
                 text={checkSumAddress}
                 onCopy={() => {
@@ -149,51 +71,64 @@ export const AddressInfoDropdown = ({
                   }, 800);
                 }}
               >
-                <div className="btn-sm !rounded-xl flex gap-3 py-3">
-                  <DocumentDuplicateIcon
-                    className="text-xl font-normal h-6 w-4 cursor-pointer ml-2 sm:ml-0"
-                    aria-hidden="true"
-                  />
-                  <span className=" whitespace-nowrap">Copy address</span>
+                <div className="flex flex-1 items-center gap-2">
+                  {!addressCopied ? (
+                    <>
+                      <DocumentDuplicateIcon className=" h-4 w-4" aria-hidden="true" />
+                      <span>Copy address</span>
+                    </>
+                  ) : (
+                    <>
+                      <CheckCircleIcon className=" h-4 w-4" aria-hidden="true" />
+                      <span>Copied</span>
+                    </>
+                  )}
                 </div>
               </CopyToClipboard>
-            )}
-          </li>
+            </Button>
+          )}
 
-          <li className={selectingNetwork ? "hidden" : ""}>
-            <label htmlFor="qrcode-modal" className="btn-sm !rounded-xl flex gap-3 py-3">
-              <QrCodeIcon className="h-6 w-4 ml-2 sm:ml-0" />
-              <span className="whitespace-nowrap">View QR Code</span>
-            </label>
-          </li>
+          {!selectingNetwork && (
+            <Button variant="ghost" className="w-full" onClick={() => setQrCodeModalOpen(true)}>
+              <div className="flex flex-1 items-center gap-2">
+                <QrCodeIcon className="h-6 w-4 ml-2 sm:ml-0" />
+                <span>View QR Code</span>
+              </div>
+            </Button>
+          )}
+          {!selectingNetwork && (
+            <Button variant="ghost" className="w-full">
+              <div className="flex flex-1 items-center justify-start gap-2">
+                <ArrowTopRightOnSquareIcon className="h-4 w-4" />
+                <a
+                  target="_blank"
+                  href={blockExplorerAddressLink}
+                  rel="noopener noreferrer"
+                  className="block"
+                >
+                  View on Block Explorer
+                </a>
+              </div>
+            </Button>
+          )}
 
-          <li className={selectingNetwork ? "hidden" : ""}>
-            <button className="menu-item btn-sm !rounded-xl flex gap-3 py-3" type="button">
-              <ArrowTopRightOnSquareIcon className="h-6 w-4 ml-2 sm:ml-0" />
-              <a
-                target="_blank"
-                href={blockExplorerAddressLink}
-                rel="noopener noreferrer"
-                className="whitespace-nowrap"
-              >
-                View on Block Explorer
-              </a>
-            </button>
-          </li>
-          {allowedNetworks.length > 1 ? (
-            <li className={selectingNetwork ? "hidden" : ""}>
-              <button
-                className="btn-sm !rounded-xl flex gap-3 py-3"
-                type="button"
-                onClick={() => {
-                  setSelectingNetwork(true);
-                }}
-              >
-                <ArrowsRightLeftIcon className="h-6 w-4 ml-2 sm:ml-0" /> <span>Switch Network</span>
-              </button>
-            </li>
-          ) : null}
-      */}
+          {allowedNetworks.length > 1 && (
+            <Button variant="ghost" className="w-full" onClick={() => setSelectingNetwork(true)}>
+              <div className="flex flex-1 items-center gap-2">
+                <ArrowsRightLeftIcon className="h-4 w-4" />
+                <span>Switch Network</span>
+              </div>
+            </Button>
+          )}
+
+          <Button variant="ghost" className="w-full" onClick={() => disconnect()}>
+            <div className="flex flex-1 items-center gap-2">
+              <LogOut className="h-4 w-4 stroke-error" />
+              <span className="text-error">Disconnect</span>
+            </div>
+          </Button>
+        </PopoverContent>
+      </Popover>
     </>
   );
 };
